1eb3b6d65e63924a70fda08d5ec046072229fbf7
zarkarliberal15 <zarkarliberal15@gmail.com>
2024-03-29 18:12:53 +0530

    Question 1 Solution

---
+++ Java_Practice_Code/src/Que1.java
@@ -0,0 +1,29 @@
+
+/*
+ *  Swap 2 variable without using 3rd variable
+ */
+public class Que1 {
+
+	public static void main(String[] args) {
+	
+		int a = 10;
+		int b = 30;
+		
+		int c;
+		
+		
+		
+		System.out.println("A: "+ a);
+		System.out.println("B: "+ b);
+		
+		System.out.println("----------After swapping---------");
+		
+		c = a;
+		a = b;
+		b = c;
+		
+		System.out.println("A: "+ a);
+		System.out.println("B: "+ b);
+	}
+
+}
