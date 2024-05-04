5d7225224ee8217e1fb94143aca90cda254fff00
zarkarliberal15 <zarkarliberal15@gmail.com>
2024-05-04 21:06:27 +0530

    enter the even number between range

---
+++ Java_Practice_Code/src/loop/Que13.java
@@ -0,0 +1,23 @@
+package loop;
+
+import java.util.Scanner;
+
+/*
+ * 13. Accept a start and end range from the user and print all even number between them.
+ */
+public class Que13 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Please enter start number");
+		int start = sc.nextInt();
+		System.out.println("Please enter end number");
+		int end = sc.nextInt();
+		sc.close();
+		for(int i=start;i<=end;i++) {
+			if(i%2==0)
+			   System.out.print(i+" ");
+		}
+	}
+
+}
